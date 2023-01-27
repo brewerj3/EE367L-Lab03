45056329e1180b9d47a22eab2365b154761bfc0c
brewerj3 <brewerj3@hawaii.edu.com>
2023-01-26 16:36:04 -1000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/EE367L_Lab03.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EE367L_Lab03.dir/main.c.o"
+  "CMakeFiles/EE367L_Lab03.dir/main.c.o.d"
+  "EE367L_Lab03"
+  "EE367L_Lab03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EE367L_Lab03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/EE367L_Lab03.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for EE367L_Lab03.
+# This may be replaced when dependencies are built.
